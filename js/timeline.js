0d9a1fe7c42b5f684ec300576a349b0ab75eeb26
Hoon Cho <soohoon90@gmail.com>
2011-11-26 19:33:33 -0500

    cname, +file

---
+++ js/timeline.js
@@ -0,0 +1,45 @@
+var timeline = {
+    entries: [
+        { 
+            title: "1A",
+            year: 2008,
+            term: 3
+        },
+        { 
+            title: "1B",
+            year: 2009,
+            term: 1
+        },
+        { 
+            title: "2A",
+            year: 2009,
+            term: 3
+        },
+        { 
+            title: "2B",
+            year: 2010,
+            term: 2
+        },
+        { 
+            title: "3A",
+            year: 2011,
+            term: 1
+        },
+        { 
+            title: "3B",
+            year: 2011,
+            term: 3
+        },
+        { 
+            title: "4A",
+            year: 2012,
+            term: 2
+        },
+
+    ]
+
+
+
+
+
+}
